af61959eeff0e880b319315e37a68cc42f85feb3
deyvigo <deyvi.gomez@unmsm.edu.pe>
2024-10-02 13:50:18 -0500

    fix: some errors

--- front/src/components/Image.jsx
+++ front/src/components/Image.jsx
@@ -1,13 +1,13 @@
 export const Image = ({ src, positionX, positionY, state }) => {
   const colors = {
     'pensando': 'text-red-500',
-    'comiendo': 'text-yellow-500',
-    'hambriento': 'text-green-500',
+    'hambriento': 'text-yellow-500',
+    'comiendo': 'text-green-500',
   }
   return (
     <>
-      <div className="absolute w-20 h-20 rounded-full" style={{ left: positionX, top: positionY }}>
-        <h1 className={ `text-center ${colors[state]}` }>{ state.toLocaleUpperCase() }</h1>
+      <div className="absolute w-20 h-20 rounded-full items-center" style={{ left: positionX, top: positionY }}>
+        <h1 className={ `text-center font-bold ${colors[state]}` }>{ state.toLocaleUpperCase() }</h1>
         <img src={src} className={ `w-full h-full object-contain rounded-full ${colors[state]}` } />
       </div>
     </>
--- one.py
+++ one.py
@@ -12,25 +12,42 @@ CORS(app)
 NUM_FILOSOFOS = 5
 ESTADOS = ["pensando", "comiendo", "hambriento"]
 filosofos_estado = ["pensando"] * NUM_FILOSOFOS
+comedor = threading.Semaphore(1)  # Semáforo para permitir que solo un filósofo coma a la vez
 mutex = threading.Semaphore(1)
 
 def filosofo(id):
   global filosofos_estado
 
   while True:
+    # Pensando
     filosofos_estado[id] = "pensando"
-    print("Filosofo %d pensando" % id)
+    print("Filósofo %d pensando" % id)
     time.sleep(2)
 
-    mutex.acquire()
-    filosofos_estado[id] = "comiendo"
-    print("Filosofo %d comiendo" % id)
-    time.sleep(2)
-
+    # Intentando comer
+    mutex.acquire()  # Proteger acceso a estados
     filosofos_estado[id] = "hambriento"
-    print("Filosofo %d ha terminado de comer y está hambriento" % id)
+    print("Filósofo %d hambriento" % id)
     mutex.release()
+
+    # Intentar comer
+    comedor.acquire()  # Esperar a que el comedor esté disponible
+
+    # Comer
+    mutex.acquire()  # Proteger acceso a estados
+    filosofos_estado[id] = "comiendo"
+    print("Filósofo %d comiendo" % id)
+    mutex.release()
+
     time.sleep(2)
+
+    # Estado después de comer
+    mutex.acquire()  # Proteger acceso a estados
+    filosofos_estado[id] = "pensando"
+    print("Filósofo %d ha terminado de comer y está pensando" % id)
+    mutex.release()
+
+    comedor.release()  # Liberar el comedor para el siguiente filósofo
 
 
 def iniciar_simulacion():
